de263695efc4187c08e2b99f7a64e04b5db931ab
Cam C <ckc.cameron@gmail.com>
2020-08-03 13:27:13 -0600

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Merchant.destroy_all
+Item.destroy_all
 
-megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
-megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+#users
+tony = User.create(name: "Tony Tiger", address: "345 Frosty St", city: "Denver",
+                  state: "CO", zip: 80205, email: "grrreat_hooray@gmail.com",
+                  password: "123test", password_confirmation: "123test", role: 0)
+
+chester = User.create(name: "Chester Cheetah", address: "455 Hot Fiyah Ave", city: "Phoenix",
+                  state: "AZ", zip: 85253, email: "hunnidmilesrunnin@gmail.com",
+                  password: "123test", password_confirmation: "123test", role: 1)
+cuckoo = User.create(name: "Sonny Cuckoo", address: "822 Cocoa River Ln", city: "Los Angeles",
+                  state: "CA", zip: 90008, email: "cuckoo4cocoa@gmail.com",
+                  password: "123test", password_confirmation: "123test", role: 1)
+
+admin = User.create(name: "Mufasa Lyons", address: "388 Pride Rock Cir", city: "San Diego",
+                  state: "CA", zip: 92102, email: "circleoflife2@gmail.com",
+                  password: "123test", password_confirmation: "123test", role: 2)
+
+#merchants
+sports = Merchant.create(name: "Santiago's Sporting Goods", address: "45 Nature Bliss St", city: "San Diego", state: "CA", zip: 92102)
+music = Merchant.create(name: "Marley's Music", address: "1 Mother Nature Cir", city: "San Diego", state: "CA", zip: 92109)
+
+#items
+sports.items.create(name: "Basketball", description: "Ballislife!", price: 30.00,
+                    image: "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F05%2Fnba-drops-spalding-basketball-for-wilson-2021-season-1.jpg?q=80&w=1000&cbr=1&fit=max",
+                    inventory: 25, active: true)
+sports.items.create(name: "Football", description: "Go long!", price: 25.00,
+                    image: "https://dks.scene7.com/is/image/GolfGalaxy/19NIKUVPR24720FFFFTB?qlt=70&wid=992&fmt=pjpeg",
+                    inventory: 20, active: true)
+music.items.create(name: "Victory Lap", description: "By Nipsey Hussle", price: 15.00,
+                    image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/46/Nipsey_Hussle_–_Victory_Lap.png/220px-Nipsey_Hussle_–_Victory_Lap.png",
+                    inventory: 10, active: true)
+music.items.create(name: "Oxnard", description: "By Anderson .Paak", price: 15.00,
+                    image: "https://consequenceofsound.net/wp-content/uploads/2018/10/anderson-paak-oxnard-album-cover-artwork.jpg",
+                    inventory: 15, active: true)
+                    
