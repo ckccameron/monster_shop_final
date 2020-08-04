7f58c0a979f8224e02ecf231766dbfb5912495e9
Cam C <ckc.cameron@gmail.com>
2020-08-04 14:28:18 -0600

    Fix user to merchant relationship for chester within index spec

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "bulk discount index" do
   describe "as merchant employee" do
     it "displays info for bulk discounts on merchant's dashboard" do
-      chester = User.create(name: "Chester Cheetah", address: "455 Hot Fiyah Ave", city: "Phoenix",
-                            state: "AZ", zip: 85253, email: "hunnidmilesrunnin@gmail.com",
-                            password: "123test", password_confirmation: "123test", role: 1)
-
       sports = Merchant.create(name: "Santiago's Sporting Goods", address: "45 Nature Bliss St", city: "San Diego", state: "CA", zip: 92102)
       music = Merchant.create(name: "Marley's Music", address: "1 Mother Nature Cir", city: "San Diego", state: "CA", zip: 92109)
+
+      chester = sports.users.create(name: "Chester Cheetah", address: "455 Hot Fiyah Ave", city: "Phoenix",
+                            state: "AZ", zip: 85253, email: "hunnidmilesrunnin@gmail.com",
+                            password: "123test", password_confirmation: "123test")
 
       sports.items.create(name: "Basketball", description: "Ballislife!", price: 30.00,
                           image: "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F05%2Fnba-drops-spalding-basketball-for-wilson-2021-season-1.jpg?q=80&w=1000&cbr=1&fit=max",
@@ -26,7 +26,7 @@ RSpec.describe "bulk discount index" do
 
       visit "/merchant"
 
-      click_on "View All Bulk Discounts"
+      click_link "View All Bulk Discounts"
       expect(current_path).to eq("/merchant/discounts")
       expect(page).to have_link(discount.name)
       expect(page).to have_content(discount.min_item_quantity)
