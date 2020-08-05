7299d49bd9425a2eaac733d8fc3fb293f764f6ad
Cam C <ckc.cameron@gmail.com>
2020-08-04 22:45:31 -0600

    Add bulk discounts method to Merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -33,4 +33,8 @@ class Merchant < ApplicationRecord
   def order_items_by_order(order_id)
     order_items.where(order_id: order_id)
   end
+
+  def bulk_discounts?
+    true if discounts.present?
+  end
 end
